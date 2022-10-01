138c93d4e89e85386c29e4f3db5bb69240230668
Matheus Oliveira <matheus.hortolandia@gmail.com>
2022-10-01 11:41:29 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterTest {
+    internal class Program {
+        static void Main (string[] args) {
+
+            string sourcePath = @"D:\Users\Matheus\Desktop\Curso - C# - Udemy\Projetos\file1.txt";
+            string targetPath = @"D:\Users\Matheus\Desktop\Curso - C# - Udemy\Projetos\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines (sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
